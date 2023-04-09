5864189c6660790305c6897746bc8e71b7a1ae67
Andydevs <akanshul97@gmail.com>
2023-04-09 18:04:16 -0400

    Add sharding algorithm (pre-concurrent)

--- main.go
+++ main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"math"
 	"math/cmplx"
 	"os"
+	"runtime"
 )
 
 // Image size
-const imageWidth = 600
-const imageHeight = 400
+const imageWidth = 1920
+const imageHeight = 1080
 
 func main() {
 	// Image
 	img := image.NewGray(image.Rect(0, 0, imageWidth, imageHeight))
 
+	// Shard image dimensions
+	shardsInX, shardsInY := shardDimensions(runtime.NumCPU())
+
+	// Show if we were to shard
+	fmt.Println("If we were to shard")
+	imgShardW := imageWidth / shardsInX
+	imgShardH := imageHeight / shardsInY
+	fmt.Printf("Image shard grid: %d by %d\n", shardsInX, shardsInY)
+	fmt.Printf("Image shard size: %d by %d\n", imgShardW, imgShardH)
+
 	// Cell calculation
 	var iters uint8
-	bounds := img.Bounds().Max
-	for i := 0; i < bounds.X; i++ {
-		for j := 0; j < bounds.Y; j++ {
+	for i := 0; i < imageWidth; i++ {
+		for j := 0; j < imageHeight; j++ {
 			iters = mandelbrot(i, j)
 			img.SetGray(i, j, color.Gray{Y: iters})
 		}
@@ -37,6 +48,23 @@ func main() {
 	err = png.Encode(f, img)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func shardDimensions(n int) (int, int) {
+	a := int(math.Sqrt(float64(n)))
+	b := n / a
+	for a*b != n {
+		if a*b < n {
+			a++
+		} else if a*b > n {
+			b--
+		}
+	}
+	if a < b {
+		return b, a
+	} else {
+		return a, b
 	}
 }
 
